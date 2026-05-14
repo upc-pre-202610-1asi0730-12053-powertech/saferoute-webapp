9767988e1b00338ca51e20f20969794e1afa13b5
Fabio Vallejo <u20211d989@upc.edu.pe>
2026-05-14 14:33:37 -0500

    feat(routes): add route service layer

--- saferoute/src/fleet-and-route-planning/infrastructure/services/route.api.js
+++ saferoute/src/fleet-and-route-planning/infrastructure/services/route.api.js
@@ -1,8 +1,6 @@
 import { BaseEndpoint } from '../../../shared/infrastructure/services/base-endpoint';
 
-/**
- * Servicio de infraestructura para gestionar rutas escolares.
- */
+
 export class RouteApi extends BaseEndpoint {
   constructor() {
     super('/routes');
