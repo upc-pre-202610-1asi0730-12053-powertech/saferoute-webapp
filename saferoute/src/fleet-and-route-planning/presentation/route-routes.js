35ff6ef82da4153d3f9973aa66c47f887bafa596
Fabio Vallejo <u20211d989@upc.edu.pe>
2026-05-14 17:25:20 -0500

    feat(routes): add route planning navigation

--- saferoute/src/fleet-and-route-planning/presentation/route-routes.js
+++ saferoute/src/fleet-and-route-planning/presentation/route-routes.js
@@ -1,8 +1,10 @@
-const routeList = () => import('./views/route-list.vue');
-const routeForm = () => import('./views/route-form.vue');
+const routeList       = () => import('./views/route-list.vue');
+const routeForm       = () => import('./views/route-form.vue');
+const routeManagement = () => import('./views/route-management.vue');
 
 export const routeRoutes = [
-   { path: 'routes',          name: 'route-routes',      component: routeList, meta: {title: 'Routes'}},
-   { path: 'routes/new',      name: 'route-route-new',   component: routeForm, meta: {title: 'New Route'}},
-   { path: 'routes/:id/edit', name: 'route-route-edit',  component: routeForm, meta: {title: 'Edit Route'}}
+   { path: 'routes',          name: 'route-routes',      component: routeList,       meta: { title: 'Routes' } },
+   { path: 'routes/new',      name: 'route-route-new',   component: routeForm,       meta: { title: 'New Route' } },
+   { path: 'routes/:id/edit', name: 'route-route-edit',  component: routeForm,       meta: { title: 'Edit Route' } },
+   { path: 'management',      name: 'route-management',  component: routeManagement, meta: { title: 'Route Management' } },
 ];
