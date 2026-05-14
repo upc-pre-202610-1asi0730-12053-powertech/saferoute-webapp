3a1483f0d915a62b3a2b40d33ecbc9a175763c9c
Fabio Vallejo <u20211d989@upc.edu.pe>
2026-05-14 16:43:28 -0500

    feat(routes): add route transform assembler

--- saferoute/src/fleet-and-route-planning/application/transform/route.assembler.js
+++ saferoute/src/fleet-and-route-planning/application/transform/route.assembler.js
@@ -1,9 +1,7 @@
 import { BaseAssembler } from '../../../shared/application/transform/base.assembler';
 import { Route } from '../../domain/model/route.entity';
 
-/**
- * Ensamblador para convertir entre DTOs y la entidad Route.
- */
+
 export class RouteAssembler extends BaseAssembler {
   toEntity(dto) {
     return new Route(dto.id, dto.name, dto.origin, dto.destination);
